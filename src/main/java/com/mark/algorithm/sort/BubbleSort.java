bf293b491e327bcf53d9ceb0b589807593f08719
sunwj <sunwj@sqbj.com>
2020-01-15 14:24:14 +0800

    添加基础排序代码

--- src/main/java/com/mark/algorithm/sort/BubbleSort.java
+++ src/main/java/com/mark/algorithm/sort/BubbleSort.java
@@ -1,9 +1,71 @@
 package com.mark.algorithm.sort;
 
+import java.util.Arrays;
+import java.util.Date;
+
 /**
  * @Author: Sunwj@sqbj.com
  * @Date: 2020/1/15 10:19 上午
  * 冒泡排序
+ * 概念：数组进行排序，两两比较相邻记录的关键字，如果反序则交换，直到没有反序的记录为止。
+ *
  */
 public class BubbleSort {
+
+    /**
+     * 正常冒泡排序
+     * 理解：
+     * @param array
+     */
+    static void bubbleSort(int[] array){
+
+        Date StartTime = new Date();
+        if (array == null || array.length <= 1) {
+            return;
+        }
+        for (int i = array.length-1; i > 0 ; i--) {
+            //当前需要进行逐一比较的值
+            for (int j = 0 ; j < i; j++) {
+                if (array[j] > array[j+1]) {
+                    int temp = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        Date endTime = new Date();
+        System.out.println("array = " + Arrays.toString(array) + "执行的时间差为" + (endTime.getTime()-StartTime.getTime()));
+    }
+
+    /**
+     * 更优的冒泡排序方法--如果经过一轮遍历发现已经是有序的就不再进行排序
+     * 理解：
+     * @param array
+     */
+    static void betterBubbleSort(int[] array){
+
+        Date StartTime = new Date();
+        if (array == null || array.length <= 1) {
+            return;
+        }
+        boolean flag = true;
+
+        for (int i = array.length-1; i >0 ; i--) {
+            flag = false;
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j+1]) {
+                    int temp = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = temp;
+                }
+                flag = true;
+            }
+            if (!flag) {
+                break;
+            }
+        }
+        Date endTime = new Date();
+        System.out.println("array = " + Arrays.toString(array)+ "执行的时间差为" + (endTime.getTime()-StartTime.getTime()));
+    }
+
 }
---
+++ src/main/java/com/mark/algorithm/sort/MainSort.java
@@ -0,0 +1,20 @@
+package com.mark.algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * @Author: Sunwj@sqbj.com
+ * @Date: 2020/1/15 11:32 上午
+ * 主的排序调用方法
+ */
+public class MainSort {
+
+    public static void main(String[] args) {
+
+        int[] array = {2,4,1,6,8,3,5,0,9,11,23,19,21,34,54,12,55,77,32};
+        //冒泡排序--正常情况
+//        BubbleSort.bubbleSort(array);
+        //更优冒泡排序
+//        BubbleSort.betterBubbleSort(array);
+    }
+}
