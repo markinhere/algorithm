8f285efb5b628c445ac7dd88a675d1692b97fe13
sunwj <sunwj@sqbj.com>
2020-01-17 14:41:34 +0800

    更新代码

--- src/main/java/com/mark/algorithm/leetcode/array/TwoSum.java
+++ src/main/java/com/mark/algorithm/leetcode/array/TwoSum.java
@@ -21,7 +21,7 @@ package com.mark.algorithm.leetcode.array;
  * 链接：https://leetcode-cn.com/problems/two-sum
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class twoSum {
+public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
 
